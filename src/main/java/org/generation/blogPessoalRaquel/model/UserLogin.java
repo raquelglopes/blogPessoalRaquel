4c7c98c040c281f3d2bdb57b04ca5eff8d6dbca3
raquel.glopes <glopes.raquel@gmail.com>
2021-05-12 07:15:12 -0300

    configuração de Entrada e Cadastro no Login

--- src/main/java/org/generation/blogPessoalRaquel/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoalRaquel/model/UserLogin.java
@@ -1,6 +1,8 @@
 package org.generation.blogPessoalRaquel.model;
 
 public class UserLogin {
+	
+	private long id;
 
 	private String nome;
 	
@@ -9,6 +11,20 @@ public class UserLogin {
 	private String senha;
 	
 	private String token;
+	
+	private String foto;
+	
+	private String tipo;
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
@@ -40,6 +56,22 @@ public class UserLogin {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 	
 	
--- src/main/java/org/generation/blogPessoalRaquel/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoalRaquel/seguranca/BasicSecurityConfig.java
@@ -29,8 +29,11 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll().anyRequest().authenticated()
+		http.authorizeRequests()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/**").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
